2a032e4ec9f6071b0606461a4b11472c29e4d1b2
Vincent Conus <vincent.conus@pm.me>
2023-07-10 11:06:34 +0900

    add the example for depth measurment

--- hello.py
+++ hello.py
@@ -1,6 +1,73 @@
-import numpy as np  # numpy - manipulate the packet data returned by depthai
-import cv2  # opencv - display the video stream
-import depthai  # depthai - access the camera and its data packets
-import blobconverter  # blobconverter - compile and download MyriadX neural network blobs
+#!/usr/bin/env python3
 
-pipeline = depthai.Pipeline()
+import cv2
+import depthai as dai
+import numpy as np
+
+# Closer-in minimum depth, disparity range is doubled (from 95 to 190):
+extended_disparity = False
+# Better accuracy for longer distance, fractional disparity 32-levels:
+subpixel = True
+# Better handling for occlusions:
+lr_check = True
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define sources and outputs
+monoLeft = pipeline.create(dai.node.MonoCamera)
+monoRight = pipeline.create(dai.node.MonoCamera)
+depth = pipeline.create(dai.node.StereoDepth)
+xout = pipeline.create(dai.node.XLinkOut)
+
+edgeDetector = pipeline.create(dai.node.EdgeDetector)
+
+xout.setStreamName("disparity")
+
+# Properties
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+monoLeft.setCamera("left")
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+monoRight.setCamera("right")
+
+# Create a node that will produce the depth map
+# (using disparity output as it's easier to visualize depth this way)
+depth.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+# Options: MEDIAN_OFF, KERNEL_3x3, KERNEL_5x5, KERNEL_7x7 (default)
+depth.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
+
+depth.setLeftRightCheck(lr_check)
+depth.setExtendedDisparity(extended_disparity)
+depth.setSubpixel(subpixel)
+
+# Linking
+monoLeft.out.link(depth.left)
+monoRight.out.link(depth.right)
+depth.disparity.link(xout.input)
+
+# Connect to device and start pipeline
+with dai.Device(pipeline) as device:
+
+    # Output queue will be used to get the disparity frames
+    # from the outputs defined above
+    q = device.getOutputQueue(name="disparity", maxSize=4, blocking=False)
+
+    while True:
+        # blocking call, will wait until a new data has arrived
+        inDisparity = q.get()
+        frame = inDisparity.getFrame()
+
+        # Normalization for better visualization
+        frame = (frame *
+                 (255 /
+                  depth.initialConfig.getMaxDisparity())).astype(np.uint8)
+
+        # Available color maps:
+        # https://docs.opencv.org/3.4/d3/d50/group__imgproc__colormap.html
+        frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
+        cv2.imshow("disparity_color", frame)
+
+        # close the CV window with the 'Q' key
+        if cv2.waitKey(1) == ord('q'):
+            cv2.destroyAllWindows()
+            break
